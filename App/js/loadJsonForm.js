b8f1f3526e97af0e232a1a7feaaf1a0206cb53f3
pjakapan <j.petchan@togetherteam.co.th>
2017-11-09 11:09:35 +0700

    sdfs

--- App/js/loadJsonForm.js
+++ App/js/loadJsonForm.js
@@ -45,7 +45,7 @@ $.ajax(getsetting).done(function (data) {
 });
 
 function loadForm(JsonFileName) {
-    var url = "/json/" + JsonFileName + ".json"
+    var url = "./json/" + JsonFileName + ".json"
     //var cdn = loadcdn();
     var json = null;
     var getsetting = {
